e1c410af71397827145a5af15448e50aefc0b3a0
MushMan19 <shubhanyuj@gmail.com>
2025-01-16 00:27:42 +0530

    Added functionality to include/exclude constant AP

--- perm.py
+++ perm.py
@@ -22,7 +22,7 @@ def generate_combinations(num:str, patterns):
     print(f'Numbers with AP: {sum(i[0] for i in correct_num_list.values())}')
 
 
-def generate_aps(numbers:str, czero):
+def generate_aps(numbers:str, czero, ccons):
     aps = set()
     
     for length in range(3, len(numbers) + 1):
@@ -30,25 +30,29 @@ def generate_aps(numbers:str, czero):
             if not czero and perm[0] == '0':
                 continue
             perm_str = ''.join(perm)
-            if is_arithmetic_progression(perm_str):
+            if is_arithmetic_progression(perm_str, ccons):
                 aps.add(perm_str)
     
     return aps
 
-def is_arithmetic_progression(s):
+def is_arithmetic_progression(s, ccons):
     nums = [int(x) for x in s]
     if len(nums) < 2:
         return False
     
     diff = nums[1] - nums[0]
+    if not ccons and diff == 0:
+        return False
     for i in range(1, len(nums) - 1):
         if nums[i+1] - nums[i] != diff:
             return False
     return True
 
 
-numbers = '0122' #Enter numbers here 
-count_zero_in_unit_place = False # if numbers like 012 that start with 0 are allowed 
-patterns = generate_aps(numbers, count_zero_in_unit_place)
+numbers = '111222333' #Enter numbers here 
+count_zero_in_unit_place = False # if numbers like 012 that start with 0 are allowed or not
+count_constant_ap = True # if constant ap like 111 is allowed or not 
+
+patterns = generate_aps(numbers, count_zero_in_unit_place, count_constant_ap)
 print(f'APs: {patterns}')
 aps = generate_combinations(numbers, patterns)
